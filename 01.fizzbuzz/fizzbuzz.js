ae3aa6a925f8767d572ef2f7547ceb22b2c52eef
yocchan-git <yocchan.business@gmail.com>
2024-01-12 11:04:40 +0900

    レビューを受け修正する

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
-let fizzbuzz = (number) => {
-  if (number % 15 == 0) {
+const fizzbuzz = (number) => {
+  if (number % 3 === 0 && number % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (number % 5 == 0) {
+  } else if (number % 5 === 0) {
     console.log("Buzz");
-  } else if (number % 3 == 0) {
+  } else if (number % 3 === 0) {
     console.log("Fizz");
   } else {
     console.log(number);
   }
 };
 
-for (let i = 1; i < 21; i++) {
+for (let i = 1; i <= 20; i++) {
   fizzbuzz(i);
 }
